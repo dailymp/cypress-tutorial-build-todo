1d64ba23ce57b6599e54a4273b1cbdd67c186a51
Daily <daily.miranda@blue-tc.com>
2020-04-10 18:21:31 +0200

    Adding integration tests

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -3,9 +3,12 @@ describe('App initialization', () => {
     cy.seedAndVisit()
 
     cy.get('.todo-list li')
-      .should('have.length', 4)
+      .should('have.length', 5)
   })
-
+  it('should contains valid html ', () => {
+    cy.contains('h1', 'todos')
+    cy.contains('.todo-list li', 'Buy Milk')
+   });
   it('Displays an error on failure', () => {
     cy.server()
     cy.route({
@@ -22,4 +25,9 @@ describe('App initialization', () => {
     cy.get('.error')
       .should('be.visible')
   })
+
+  it('should add new todos', () => {
+    cy.get(`.new-todo`).type('first-item{enter}')
+    cy.contains('.todo-list li', 'first-item').should('be.visible')
+  });
 })
